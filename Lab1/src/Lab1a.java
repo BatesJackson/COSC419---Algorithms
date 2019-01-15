c7c718d6e571419cd8c01998a6739ce7ae84c3b5
BatesJackson <32694371+BatesJackson@users.noreply.github.com>
2019-01-14 16:01:49 -0800

    Lab1-a Complete

--- Lab1/src/Lab1a.java
+++ Lab1/src/Lab1a.java
@@ -1,30 +1,35 @@
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class Lab1a {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int dwCount, apartments, width, length, height, area, total;
-        int dwHeight, dwWidth, dwArea;
+        float dwCount, apartments, width, length, height, area;
+        float dwHeight, dwWidth, dwArea;
+        float total;
         boolean run = true;
-        dwArea = 0;
+        
         while(run){
+            dwArea = 0;
             apartments = sc.nextInt();
             width = sc.nextInt();
             length = sc.nextInt();
             height = sc.nextInt();
             area = sc.nextInt();
             dwCount = sc.nextInt();
-            if(width == 0) run = false;
+            if(width == 0) break;
             for(int j = 0; j < dwCount; j++){
                 dwHeight = sc.nextInt();
+                //System.out.println("Height: " + dwHeight);
                 dwWidth = sc.nextInt();
+                //System.out.println("Width: " + dwWidth);
                 dwArea = dwArea + (dwHeight * dwWidth);
+                //System.out.println(dwArea);
             }
-            total = (apartments * (((length * height * 2) + (width * height * 2) + (length * width)) - dwArea)) / area;
-        
-            System.out.println(total);
+            total = (apartments * (((length * height * 2) + (width * height * 2) + (length * width)) - dwArea)) / area; 
+            System.out.println((int)Math.ceil(total));
         }
     }
 }
