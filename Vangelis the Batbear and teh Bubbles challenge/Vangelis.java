409133cb9d7187ce066103def28568812fbb33dc
BatesJackson <32694371+BatesJackson@users.noreply.github.com>
2019-02-05 16:57:38 -0800

    Update Vangelis.java

--- Vangelis the Batbear and teh Bubbles challenge/Vangelis.java
+++ Vangelis the Batbear and teh Bubbles challenge/Vangelis.java
@@ -34,7 +34,6 @@ class Main {
 class Graph{
     private int numOfVerticies;
     private LinkedList<Integer> adj[];
-    
     public Graph(int numOfVerticies){
         this.numOfVerticies = numOfVerticies;
         adj = new LinkedList[numOfVerticies];
@@ -42,7 +41,6 @@ class Graph{
             adj[i] = new LinkedList();
         }
     }
-    
     public void addEdge(int v, int w){
         adj[v].add(w);
         adj[w].add(v);
@@ -50,54 +48,38 @@ class Graph{
     public int getNumOfVerticies(){
         return numOfVerticies;
     }
-    
-    public LinkedList<Integer> getAdjList(int vertex){
+        public LinkedList<Integer> getAdjList(int vertex){
         return adj[vertex];
     }
-    
         public int DFS(int vertex){
         Stack<Integer> stack = new Stack<Integer>();
         ArrayList<Integer> visited = new ArrayList();
         int count = 0;
-        //stack.add(vertex);
-        //visited.add(vertex);
         boolean[] isConnected = new boolean[this.numOfVerticies];
         int connectionCount  = 0;
         for(int i = 0; i < this.numOfVerticies; i++){
             List<Integer> list = this.getAdjList(i);
             if(list.size() > 0) isConnected[i] = true; 
-            //connectionCount++;
-            //System.out.println(isConnected[i]);
         }
         int i = 0;
         while(i < this.numOfVerticies){
             if(isConnected[i]){
-                //count = i;
-                //poo
-                //System.out.println("Using Index " + i);
                 stack.add(i);
                 visited.add(i);
                 i++;
             }
             else {
-                //System.out.println("Continuing from index " + i);
                 i = i + 1;
                 continue;
             }
             while(!stack.isEmpty()){
                 int current = stack.pop();
-                //System.out.println("Popping: " + current);
                 List<Integer> list = this.getAdjList(current);
-                //System.out.println("Current NOde: " + current);
-                //System.out.println (list);
                 for(Integer v : list){
                     if(current == v) return 1;
-                    //System.out.println("Attempts to visit: " + v);
                     if(!visited.contains(v)){
-                        //System.out.println("Vists: " + v);
                         stack.add(v);
                         visited.add(v);
-                        //System.out.println("Count: " + count);
                     }
                     else if(stack.contains(v)){
                         return 1;
